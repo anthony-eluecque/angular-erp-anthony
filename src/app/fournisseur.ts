3e933104b48e37ef1c831debfb3501db9c75ee28
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-09 15:16:59 +0100

    fournisseur json

---
+++ src/app/fournisseur.ts
@@ -0,0 +1,116 @@
+export interface Fournisseur {
+id: number;
+name: string;
+}
+export const fournisseurs = [
+{
+id: 401000075,
+name: 'BOSCH',
+student:'DESQUENNE'
+},
+{
+id: 401000212,
+name: 'ACER',
+student:'DESSAILLY'
+},
+{
+id: 401000221,
+name: 'AMD',
+student:'DOURNEL'
+},
+{
+id: 401000204,
+name: 'APPLE',
+student:'CORION'
+},
+{
+id: 401000202,
+name: 'ASUS',
+student:'HAFFNER'
+},
+{
+id: 401000216,
+name: 'DELL',
+student:'CZARKOWSKI'
+},
+{
+id: 401000249,
+name: 'DOUR',
+student:'DOURNEL'
+},
+{
+id: 401000210,
+name: 'FENDER',
+student:'AMZIL'
+},
+{
+id: 401000207,
+name: 'HUAWEI',
+student:'GUARIM'
+},
+{
+id: 401000237,
+name: 'HUAWEI',
+student:'VASSEUR'
+},
+{
+id: 401000220,
+name: 'LENOVO',
+student:'PAP'
+},
+{
+id: 401000203,
+name: 'LG',
+student:'PARENT'
+},
+{
+id: 401000214,
+name: 'LOGITECH',
+student:'LANGREZ'
+},
+{
+id: 401000218,
+name: 'MARSHALL',
+student:'LEFUR'
+},
+{
+id: 401000219,
+name: 'MICROSOFT',
+student:'CUMZAIN'
+},
+{
+id: 401000211,
+name: 'NVIDIA',
+student:'ELUECQUE'
+},
+{
+id: 401000208,
+name: 'RAZER',
+student:'BACQUET'
+},
+{
+id: 401000209,
+name: 'SAMSUNG',
+student:'BERNARD'
+},
+{
+id: 401000217,
+name: 'SONY',
+student:'PRUVOST'
+},
+{
+id: 401000205,
+name: 'STEELSERIES',
+student:'PRUVOST'
+},
+{
+id: 401000213,
+name: 'TCL',
+student:'DUVIEUBOURG'
+},
+{
+id: 401000215,
+name: 'VANUXEEM',
+student:'LORIDAN'
+}
+];
