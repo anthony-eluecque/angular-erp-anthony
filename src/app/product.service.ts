2ac47a9f7645ec9a8cb2063c73c8496d40ac302c
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-09 15:17:43 +0100

    edit fournisseur id and getProducts method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,12 +8,15 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class ProductService {
   private productsUrl = 'http://80.11.122.222:8000/polyfx/cgi/getart.cgi?frs=';
-  private fournisseurId: string = '401000075'; //< !> ICI mettre votre fournisseur à vous !!
+  private fournisseurId: string = '401000211'; //< !> ICI mettre votre fournisseur à vous !!
   private products: Product[] = [];
 
   constructor(private http: HttpClient) {}
 
-  // getProducts(): Observable<Product[]> {
-
-  // }
+  getProducts(): Observable<Product[]> {
+    // if (this.products.length>0){
+      // return this.products;
+    // }
+    return this.http.get<any>(this.productsUrl+this.fournisseurId);
+  }
 }
