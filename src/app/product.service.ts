58c7b20a18e4f12a7af91e1983f98d961c68dc4e
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-09 14:26:33 +0100

    4 files modified, 14 added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private productsUrl = 'http://80.11.122.222:8000/polyfx/cgi/getart.cgi?frs=';
+  private fournisseurId: string = '401000075'; //< !> ICI mettre votre fournisseur à vous !!
+  private products: Product[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // getProducts(): Observable<Product[]> {
+
+  // }
 }
